29a4b13adb59ec72d7a563cc81476b0ead329fcd
Anna Qiu <uhjfh@student.kit.edu>
2021-10-03 10:51:42 +0200

    parseStringToUInt and evaluation_attribute

--- contracts/MediSystem.sol
+++ contracts/MediSystem.sol
@@ -56,8 +56,62 @@ contract MediSystem {
         InterfaceMediCoin medicoin = InterfaceMediCoin(mediCoinAddress);
         return medicoin.balanceOf(doctors[msg.sender].doctorAccount);
     }
+
     function getDoctorName(address _doctorAddress) public view isOwner returns(string memory) {
         return doctors[_doctorAddress].doctorName;
+    }
+
+    function evaluation_attribute(uint _attributeAmount) public view returns(uint){
+        uint attributeCredit;
+        if(_attributeAmount <= 6){
+            attributeCredit  = 0;
+        }else if(6 < _attributeAmount && _attributeAmount <= 12){
+            attributeCredit = 20;
+        }else if(12 < _attributeAmount && _attributeAmount <= 18){
+            attributeCredit = 40;
+        }else if(18< _attributeAmount && _attributeAmount <= 24){
+            attributeCredit = 60;
+        }else if(24 < _attributeAmount && _attributeAmount <= 30){
+            attributeCredit = 80;
+        }else{
+            attributeCredit = 100;
+        }
+        return attributeCredit;
+    }
+
+    function parseStringToUint(string memory _string) public view returns(uint){
+        uint[] memory array1 = new uint[](10);
+        uint uint8_number = 48;
+
+        uint[] memory array2 = new uint[](bytes(_string).length);
+        uint len_array2 = array2.length;
+        uint number = 0;
+
+        for(uint y = 0; y < 10; y++){
+            array1[y] = uint8_number;
+            uint8_number += 1;
+        }
+
+        for(uint i = 0; i < bytes(_string).length; i++){
+            bytes1 new_string = bytes(_string)[i];
+            uint num = uint8(new_string);
+            array2[i] = num;
+        }
+
+        for(uint p = 0; p < 10; p++){
+            for(uint r = 0; r < len_array2; r++){
+                if(array1[p] == array2[r]){
+                    array2[r] = p;
+                }
+            }
+        }
+
+        for(uint e = 0; e < len_array2; e++){
+            array2[e] = array2[e] * 10 ** (len_array2-1-e);
+            number = number + array2[e];
+        }
+
+        return number;
     }
 
 }
