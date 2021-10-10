1ade6b48d360a2def5748faebbe0e1ce7ab54c48
maximilian.glimm <ujhhs@student.kit.edu>
2021-10-10 21:39:04 +0200

    Update Admin page

--- Frontend/src/Pages/ContributeDataPage/Components/contributeDataPageDataSetValueDetailsPage.tsx
+++ Frontend/src/Pages/ContributeDataPage/Components/contributeDataPageDataSetValueDetailsPage.tsx
@@ -104,7 +104,6 @@ export const ContributeDataPageDataSetValueDetailsPage = () => {
                         <Row
                             styleProps={{
                                 width: "100%",
-                                //background:"red"
                             }}
                         >
                             <Button
